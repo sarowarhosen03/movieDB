2c342c70d04cfa79442681546dbaa18c954377d6
sarowarhosen03 <md.sarowar.hosse@gmail.com>
2024-04-26 16:31:11 +0600

    for finding emergency ticket

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,8 +1,9 @@
 'use client';
-import {usePathname, useRouter} from "next/navigation";
+import redirectHard from "@/lib/hardRedrect";
+import { locales } from "@/middleware";
 import Image from "next/image";
-import {useCallback, useEffect, useRef, useState} from "react";
-import {locales} from "@/middleware";
+import { usePathname, useRouter } from "next/navigation";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function LanguageSwitcher() {
     const router = useRouter();
@@ -27,17 +28,14 @@ function LanguageSwitcher() {
         //i can parsist it via server-side cookie due to courese limitiation i cant make any api call
         const newPathName = pathname.replace(`/${currentLangPath}`, `/${lang.code}`);
         setSelectedLanguage(lang);
-        if (pathname.includes("movies")) window.location.replace(newPathName) //to bypassRouteInterception
-        else router.push(newPathName, {
-            scroll: false
-        });
         toggleMenu()
+        redirectHard(newPathName);
     }
 
-    const  escapeMenu= useCallback((e)=>{
-        if (e.key === "Escape"&&!langDropDwonRef.current?.classList.contains("hidden")) toggleMenu()
+    const escapeMenu = useCallback((e) => {
+        if (e.key === "Escape" && !langDropDwonRef.current?.classList.contains("hidden")) toggleMenu()
 
-    },[])
+    }, [])
 
     useEffect(() => {
         document.addEventListener('keydown', escapeMenu)
@@ -49,25 +47,25 @@ function LanguageSwitcher() {
     return (<div className="relative">
         <button onClick={toggleMenu} className="flex items-center gap-2">
             <Image className="max-w-8" src={selectedLanguage.image} height={20} width={20}
-                   alt={selectedLanguage.image}/>
+                alt={selectedLanguage.image} />
             {selectedLanguage.language}
         </button>
 
-        <div  ref={langDropDwonRef} className="hidden">
-            <div onClick={toggleMenu}  className="fixed inset-0  h-screen w-screen  "></div>
+        <div ref={langDropDwonRef} className="hidden">
+            <div onClick={toggleMenu} className="fixed inset-0  h-screen w-screen  "></div>
 
             <div
-                 className="absolute right-0 top-full mt-2 w-40 rounded-md bg-amber-50 dark:bg-body p-2 z-10 shadow-lg  ">
+                className="absolute right-0 top-full mt-2 w-40 rounded-md bg-amber-50 dark:bg-body p-2 z-10 shadow-lg  ">
                 <ul>
                     {languages?.filter((lang) => lang.code !== found?.code)
                         .map((lang, index) => (<li key={index}
-                                                   onClick={() => handelSwithcLanguage(lang)}
-                                                   className="flex items-center justify-between gap-2 p-2 rounded-md cursor-pointer dark:hover:text-body  hover:bg-primary ">
+                            onClick={() => handelSwithcLanguage(lang)}
+                            className="flex items-center justify-between gap-2 p-2 rounded-md cursor-pointer dark:hover:text-body  hover:bg-primary ">
 
-                                <Image className="max-w-8" src={lang.image} alt={lang.image} height={20} width={20}/>
-                                {lang.language}
+                            <Image className="max-w-8" src={lang.image} alt={lang.image} height={20} width={20} />
+                            {lang.language}
 
-                            </li>
+                        </li>
 
                         ))}
                 </ul>
